de8ab3ab1c4cc93ae07f072e02afbf2cb6ca5b08
FedeAle1525 <fedeale.1525@gmail.com>
2023-03-30 15:12:14 +0200

    PHP create Class Movie whit variables, constructor ... ... and method

--- Movie.php
+++ Movie.php
@@ -1,5 +1,24 @@
 <?php
-
 class Movie
 {
+  // Definisco gli attributi della Classe
+  public string $title;
+  public string $genre;
+  public int $time_min;
+
+  // Creo il Costruttore che verra' richiamato nel momento in cui verra creata un Instanza della Classe
+  // Definisco come Parametro del Costruttore il Titolo che dovro' definire (obbligatoriamente) alla creazione dell'Istanza
+  function __construct($_title)
+  {
+    $this->title = $_title;
+  }
+
+  public function getTimeHour()
+  {
+    $num_hour = floor($this->time_min / 60);
+
+    $num_min = $this->time_min - ($num_hour * 60);
+
+    return "$num_hour h e $num_min m";
+  }
 }
--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+// Importo la Classe "Movie"
+require_once __DIR__ . '/./Movie.php';
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
